a18969be2f80b8aa01b48e84477bdb02742a3dc3
TheBlackParrot <theblackparrot0@gmail.com>
2015-10-28 01:20:47 -0500

    loading freezes on servers with RTB, fixed

--- handshake.cs
+++ handshake.cs
@@ -1,12 +1,12 @@
 package BLPrefServerPackage {
-	function GameConnection::onConnectRequest(%this, %a, %b, %c, %d, %e, %f, %us, %h, %i, %j, %k, %l, %m, %n, %o, %p) {
+	function GameConnection::onConnectRequest(%this, %a, %b, %c, %d, %e, %f, %g, %us, %i, %j, %k, %l, %m, %n, %o, %p) {
 		if(getField(%us, 0) !$= "") {
 			%this.hasPrefSystem = 1;
 			if($Pref::BLPrefs::ServerDebug) {
 				echo("\c4" @ %this.name SPC "has the pref system. (version" SPC getField(%us, 0) @ ")");
 			}
 		}
-		return parent::onConnectRequest(%this, %a, %b, %c, %d, %e, %f, %us, %h, %i, %j, %k, %l, %m, %n, %o, %p);
+		return parent::onConnectRequest(%this, %a, %b, %c, %d, %e, %f, %g, %us, %i, %j, %k, %l, %m, %n, %o, %p);
 	}
 };
 activatePackage(BLPrefServerPackage);
