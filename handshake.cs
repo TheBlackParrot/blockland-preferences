cd38f3a847114b96ba5e80f64ba35dba53099f29
Jincux <dephurites@gmail.com>
2015-10-30 15:46:08 -0400

    use connect args more conservatively

--- handshake.cs
+++ handshake.cs
@@ -1,9 +1,12 @@
 package BLPrefServerPackage {
 	function GameConnection::onConnectRequest(%this, %a, %b, %c, %d, %e, %f, %g, %us, %i, %j, %k, %l, %m, %n, %o, %p) {
-		if(getField(%us, 0) !$= "") {
-			%this.hasPrefSystem = 1;
-			if($Pref::BLPrefs::ServerDebug) {
-				echo("\c4" @ %this.name SPC "has the pref system. (version" SPC getField(%us, 0) @ ")");
+		for(%i = 0; %i < getLineCount(%us); %i++) { //being respectful of other mods, not hogging a whole argument
+			%line = getLine(%us, %i);
+			if(getField(%line, 0) $= "Prefs") {
+				%this.hasPrefSystem = 1;
+				if($Pref::BLPrefs::ServerDebug) {
+					echo("\c4" @ %this.name SPC "has the pref system. (version" SPC getField(%line, 1) @ ")");
+				}
 			}
 		}
 		return parent::onConnectRequest(%this, %a, %b, %c, %d, %e, %f, %g, %us, %i, %j, %k, %l, %m, %n, %o, %p);
