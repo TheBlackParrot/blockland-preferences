90c36488496cbab4ec3862ebdacb9008795de5f3
Joshua Olive <joshmolive@gmail.com>
2016-10-20 19:37:58 +0100

    Revert "orbs support removal"
    This reverts commit 89f147fa95b9a9db2437cb10c2a8124d19806f2c.

--- server/compatibility.cs
+++ server/compatibility.cs
@@ -4,10 +4,14 @@ if(isFile("Add-Ons/System_ReturnToBlockland/hooks/serverControl.cs"))
 	exec("Add-Ons/System_ReturnToBlockland/hooks/serverControl.cs");
 $RTB::Hooks::ServerControl = true; // RTB is totally on guys
 
+if(isFile("Add-Ons/System_oRBs/hooks/serverControl.cs"))
+	exec("Add-Ons/System_oRBs/hooks/serverControl.cs");
+$ORBS::Hooks::ServerControl = true; // yup oRBs too
+
 if(!isFile("Add-Ons/System_ReturnToBlockland/server.cs")) { // the addon does not need to be *valid*, the server.cs just needs to exist
 	%fo = new FileObject();
 	%fo.openForWrite("Add-Ons/System_ReturnToBlockland/server.cs");
-	%fo.writeLine("// This is an empty, fake RTB folder so that RTB prefs will load."); // if you're going to change this, ensure file CRC is changed on line 49
+	%fo.writeLine("// This is an empty, fake RTB folder so that RTB prefs will load."); // if you're going to change this, ensure file CRC is changed on line 63
 	%fo.close();
 	%fo.delete();
 }
@@ -42,11 +46,23 @@ package BLPrefCompatibilityPackage {
 		// some prefs have "$", some don't
 		registerPref(%cat, %sub, %name, %type, "$" @ strReplace(%variable, "$", ""), %filename, %default, getWords(%params, 1), %callback, 1);
 	}
+
+	// because oRBs is the same thing at this point -_-
+	// so much for "innovation", amirite?
+	function oRBs_registerPref(%name, %addon, %variable, %params, %filename, %default, %requiresRestart, %hostOnly, %callback) {
+		RTB_registerPref(%name, %addon, %variable, %params, %filename, %default, %requiresRestart, %hostOnly, %callback);
+
+		if(isFunction("oRBs_registerPref")) {
+			parent::oRBs_registerPref(%name, %addon, %variable, %params, %filename, %default, %requiresRestart, %hostOnly, %callback);
+		}
+	}
 	
 	function ServerSettingsGui::onWake(%this) {
 		parent::onWake(%this);
-    
-		if(getFileCRC("Add-Ons/System_ReturnToBlockland/server.cs") == -1587284330) {
+		
+		%crc = "-1587284330";
+		
+		if(getFileCRC("Add-Ons/System_ReturnToBlockland/server.cs") $= %crc) {
 			$ServerSettingsGui::UseRTB = false;
 			ServerSettingsGui_RTBLabel.setVisible(false);
 			ServerSettingsGui_UseRTB.setVisible(false);
